1c641a4b9c95e39200720d27696a668223e5e60e
Fregammo <Vandyshev_s_00@mail.ru>
2023-04-12 19:02:04 +0300

    process

--- AZS/src/Car.java
+++ AZS/src/Car.java
@@ -6,31 +6,28 @@ public class Car {
     private int fuel;
     private int timeArriving;
     private int timeRefuel;
+
     public Car( int timeArriving, int timeRefuel){
         Random rand = new Random();
-        tank_fullness = rand.nextInt(200) + 1;
-        //tank_fullness= (int) (Math.random()*20+40);
-        tank_capacity = Math.min(tank_fullness, rand.nextInt(200));
-        //tank_capacity=(int)(Math.random()*tank_fullness);
-        fuel = Math.min(rand.nextInt(200) + 1, tank_fullness - tank_capacity);
-        //fuel=(int)(Math.random()*tank_fullness+1);
+        tank_fullness = rand.nextInt(20) + 20;
+        tank_capacity = rand.nextInt(tank_fullness);
+        fuel = rand.nextInt( tank_fullness - tank_capacity);
         this.timeArriving=timeArriving;
         this.timeRefuel=timeRefuel;
     }
+
     public int getTimeArriving(){
         return timeArriving;
     }
+
     public int getTimeRefuel(){
         return timeRefuel;
     }
+
     public int getFuel(){
-        if (tank_capacity+fuel>tank_fullness){
-            return tank_fullness;
-        }
-        else {
-            return fuel;
-        }
+        return fuel;
     }
+
     public void setTimeRefuel(int i){
         timeRefuel=i;
     }
--- AZS/src/Main.java
+++ AZS/src/Main.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.concurrent.ThreadLocalRandom;
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        Imitation imitation = new Imitation();
-        imitation.start();
+    public static void main(String[] args)  {
     }
 }
